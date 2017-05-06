40f1e7f4d00855595daf5d69e012bdf44b15e924
JensDiemer <git@jensdiemer.de>
2017-05-06 18:10:13 +0200

    fixup! update classifiers

--- setup.py
+++ setup.py
@@ -43,15 +43,15 @@ exec(read('pyeditor', 'version.py'))
 classifiers = """
 Development Status :: 2 - Pre-Alpha
 Intended Audience :: Developers
-License :: OSI Approved :: GPL v3+
-#Operating System :: MacOS :: MacOS X
-#Operating System :: Microsoft :: Windows
+License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
 Operating System :: POSIX
 Programming Language :: Python
-#Programming Language :: Python :: 3
-#Programming Language :: Python :: 3.4
-#Programming Language :: Python :: 3.5
-#Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Topic :: Internet
 """
 
