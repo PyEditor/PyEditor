5ebc82ab7c9fe00276237fa9fdaee9e97d2df607
JensDiemer <git@jensdiemer.de>
2017-05-07 13:17:13 +0200

    add "publish"

--- setup.py
+++ setup.py
@@ -1,17 +1,167 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
-from __future__ import unicode_literals
 
 import distutils
+import os
+import sys
 import subprocess
-from os.path import dirname, join
+import shutil
 
 from setuptools import setup, find_packages
 
 
+if sys.version_info[0] < 3:
+    print("ERROR: Python 3 in needed. You use Python %s" % sys.version)
+    sys.exit(1)
+
+
 def read(*args):
-    return open(join(dirname(__file__), *args)).read()
+    return open(os.path.join(os.path.dirname(__file__), *args)).read()
+
+
+# __version__
+exec(read('pyeditor', 'version.py'))
+
+
+if "publish" in sys.argv:
+    """
+    'publish' helper for setup.py
+
+    Build and upload to PyPi, if...
+        ... __version__ doesn't contains "dev"
+        ... we are on git 'master' branch
+        ... git repository is 'clean' (no changed files)
+
+    Upload with "twine", git tag the current version and git push --tag
+
+    The cli arguments will be pass to 'twine'. So this is possible:
+     * Display 'twine' help page...: ./setup.py publish --help
+     * use testpypi................: ./setup.py publish --repository=test
+
+    TODO: Look at: https://github.com/zestsoftware/zest.releaser
+
+    Source: https://github.com/jedie/python-code-snippets/blob/master/CodeSnippets/setup_publish.py
+    copyleft 2015-2016 Jens Diemer - GNU GPL v2+
+    """
+    if sys.version_info[0] == 2:
+        input = raw_input
+
+    import_error = False
+    try:
+        # Test if wheel is installed, otherwise the user will only see:
+        #   error: invalid command 'bdist_wheel'
+        import wheel
+    except ImportError as err:
+        print("\nError: %s" % err)
+        print("\nMaybe https://pypi.python.org/pypi/wheel is not installed or virtualenv not activated?!?")
+        print("e.g.:")
+        print("    ~/your/env/$ source bin/activate")
+        print("    ~/your/env/$ pip install wheel")
+        import_error = True
+
+    try:
+        import twine
+    except ImportError as err:
+        print("\nError: %s" % err)
+        print("\nMaybe https://pypi.python.org/pypi/twine is not installed or virtualenv not activated?!?")
+        print("e.g.:")
+        print("    ~/your/env/$ source bin/activate")
+        print("    ~/your/env/$ pip install twine")
+        import_error = True
+
+    if import_error:
+        sys.exit(-1)
+
+    def verbose_check_output(*args):
+        """ 'verbose' version of subprocess.check_output() """
+        call_info = "Call: %r" % " ".join(args)
+        try:
+            output = subprocess.check_output(args, universal_newlines=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            print("\n***ERROR:")
+            print(err.output)
+            raise
+        return call_info, output
+
+    def verbose_check_call(*args):
+        """ 'verbose' version of subprocess.check_call() """
+        print("\tCall: %r\n" % " ".join(args))
+        subprocess.check_call(args, universal_newlines=True)
+
+    if "dev" in __version__:
+        print("\nERROR: Version contains 'dev': v%s\n" % __version__)
+        sys.exit(-1)
+
+    print("\nCheck if we are on 'master' branch:")
+    call_info, output = verbose_check_output("git", "branch", "--no-color")
+    print("\t%s" % call_info)
+    if "* master" in output:
+        print("OK")
+    else:
+        print("\nNOTE: It seems you are not on 'master':")
+        print(output)
+        if input("\nPublish anyhow? (Y/N)").lower() not in ("y", "j"):
+            print("Bye.")
+            sys.exit(-1)
+
+    print("\ncheck if if git repro is clean:")
+    call_info, output = verbose_check_output("git", "status", "--porcelain")
+    print("\t%s" % call_info)
+    if output == "":
+        print("OK")
+    else:
+        print("\n *** ERROR: git repro not clean:")
+        print(output)
+        sys.exit(-1)
+
+    print("\ncheck if pull is needed")
+    verbose_check_call("git", "fetch", "--all")
+    call_info, output = verbose_check_output("git", "log", "HEAD..origin/master", "--oneline")
+    print("\t%s" % call_info)
+    if output == "":
+        print("OK")
+    else:
+        print("\n *** ERROR: git repro is not up-to-date:")
+        print(output)
+        sys.exit(-1)
+    verbose_check_call("git", "push")
+
+    print("\nCleanup old builds:")
+    def rmtree(path):
+        path = os.path.abspath(path)
+        if os.path.isdir(path):
+            print("\tremove tree:", path)
+            shutil.rmtree(path)
+    rmtree("./dist")
+    rmtree("./build")
+
+    print("\nbuild but don't upload...")
+    log_filename="build.log"
+    with open(log_filename, "a") as log:
+        call_info, output = verbose_check_output(
+            sys.executable or "python",
+            "setup.py", "sdist", "bdist_wheel", "bdist_egg"
+        )
+        print("\t%s" % call_info)
+        log.write(call_info)
+        log.write(output)
+    print("Build output is in log file: %r" % log_filename)
+
+    print("\ngit tag version (will raise a error of tag already exists)")
+    verbose_check_call("git", "tag", "v%s" % __version__)
+
+    print("\nUpload with twine:")
+    twine_args = sys.argv[1:]
+    twine_args.remove("publish")
+    twine_args.insert(1, "dist/*")
+    print("\ttwine upload command args: %r" % " ".join(twine_args))
+    from twine.commands.upload import main as twine_upload
+    twine_upload(twine_args)
+
+    print("\ngit push tag to server")
+    verbose_check_call("git", "push", "--tags")
+
+    sys.exit(0)
 
 
 class ToxTestCommand(distutils.cmd.Command):
@@ -37,8 +187,6 @@ class ToxTestCommand(distutils.cmd.Command):
         self.announce("Running tests with 'tox'...", level=distutils.log.INFO)
         return subprocess.call(['tox'])
 
-
-exec(read('pyeditor', 'version.py'))
 
 classifiers = """
 Development Status :: 2 - Pre-Alpha
